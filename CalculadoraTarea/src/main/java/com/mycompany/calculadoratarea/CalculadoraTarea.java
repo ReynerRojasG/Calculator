6d9ee75a5993d66d1b514a71f45e80a9dd93d320
ReynerRojasG <reyner.rojas.gutierrez@est.una.ac.cr>
2024-03-06 16:37:56 -0600

    Calculadora

---
+++ CalculadoraTarea/src/main/java/com/mycompany/calculadoratarea/CalculadoraTarea.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoratarea;
+
+/**
+ *
+ * @author Reyner
+ */
+public class CalculadoraTarea {
+
+    public static void main(String[] args) {
+        
+            Calculadora calculator = new Calculadora();
+            calculator.setVisible(true);
+        
+    }
+}
